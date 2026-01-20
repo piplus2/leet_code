cd7259cc3e1e14fd7f522916810fed5255c4a2bf
pinglese <p.inglese@outlook.com>
2026-01-20 10:39:15 +0100

    📝 improved header

--- easy/3314_min_bitwise_array_i/main.cpp
+++ easy/3314_min_bitwise_array_i/main.cpp
@@ -1,3 +1,26 @@
+/*
+We consider the binary sequence of ans and ans + 1, made of n binary digits.
+
+If we have a block of 1's, and we add a 1 to the least significant
+position we have:
+
+01111...1 + 00000...1 = 10000...0 (n+1 elements)
+
+Adding 1 to the block transforms all 1 bits to 0 with the extra bit 1.
+If we do the OR of the first term and the sum we have:
+
+10000...0 v 01111...1 = 11111...1 (n+1 elements)
+
+a sequence of n+1 1's.
+
+Thus the OR operation on ans and ans+1 corresponds to getting a block
+of n+1 1's if we had a block of n 1's in ans.
+Inverting this reasoning, we can get ans by looking at the longest block of 1's
+from the least significant bit. By setting the most significant bit of this
+block to 0, we perform the inverse of the operation seen above.
+This corresponds to the largest value such that OR(ans, and + 1) = nums[i].
+*/
+
 #include <vector>
 #include <sstream>
 #include <iostream>
