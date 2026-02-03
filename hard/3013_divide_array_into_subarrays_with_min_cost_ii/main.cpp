2d1e332d340301b1ac3e44fceb2bd0deacee7887
pinglese <p.inglese@outlook.com>
2026-02-03 09:21:42 +0100

    problem 3013 - using atomic ops with consistent state

--- hard/3013_divide_array_into_subarrays_with_min_cost_ii/main.cpp
+++ hard/3013_divide_array_into_subarrays_with_min_cost_ii/main.cpp
@@ -11,102 +11,97 @@ Return the minimum possible sum of the cost of these subarrays.
 #include <vector>
 #include <set>
 #include <algorithm>
-#include <iostream>
-#include <ios>
 
 using namespace std;
 
-// Trucco per velocizzare I/O in C++
-auto fast_io = []()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    return 0;
-}();
-
 class Solution
 {
+private:
+    void add(int x, multiset<int> &set1, multiset<int> &set2, long long &cost, int L)
+    {
+        if (L <= 0)
+            return;
+
+        if (set1.size() < L)
+        {
+            set1.insert(x);
+            cost += x;
+        }
+        else if (x < *set1.rbegin())
+        {
+            set1.insert(x);
+            cost += x;
+            auto it = prev(set1.end());
+            cost -= *it;
+            set2.insert(*it);
+            set1.erase(it);
+        }
+        else
+        {
+            set2.insert(x);
+        }
+    }
+
+    void
+    remove(int x, multiset<int> &set1, multiset<int> &set2, long long &cost, int L)
+    {
+        if (L <= 0)
+            return;
+
+        auto it = set1.find(x);
+        if (it != set1.end())
+        {
+            cost -= x;
+            set1.erase(it);
+            if (!set2.empty())
+            {
+                auto it2 = set2.begin();
+                cost += *it2;
+                set1.insert(*it2);
+                set2.erase(it2);
+            }
+        }
+        else
+        {
+            auto it2 = set2.find(x);
+            if (it2 != set2.end())
+                set2.erase(it2);
+        }
+    }
+
 public:
     long long minimumCost(vector<int> &nums, int k, int dist)
     {
         int n = nums.size();
-        int m = k - 2; // Quanti ne servono nel left_set
+        int m = k - 1;
         long long cost_0 = nums[0];
 
-        multiset<pair<int, int>> left_set, right_set;
+        multiset<int> left_set, right_set;
         long long curr_sum = 0;
 
-        // Inizializzazione finestra per i=1
-        for (int j = 2; j <= min(n - 1, 1 + dist); ++j)
+        int window_size = min(n - 1, dist + 1);
+        vector<int> window(nums.begin() + 1, nums.begin() + 1 + window_size);
+        nth_element(window.begin(), window.begin() + m, window.end());
+        for (int i = 0; i < window.size(); ++i)
         {
-            right_set.insert({nums[j], j});
-        }
-
-        // Popoliamo left_set (Squadra A)
-        while (left_set.size() < m && !right_set.empty())
-        {
-            auto it = right_set.begin();
-            curr_sum += it->first;
-            left_set.insert(*it);
-            right_set.erase(it);
-        }
-
-        long long min_total_cost = cost_0 + nums[1] + curr_sum;
-
-        for (int i = 2; i <= n - k + 1; ++i)
-        {
-            // 1. Rimuoviamo nums[i] che diventa il nuovo perno
-            pair<int, int> toRemove = {nums[i], i};
-            auto itL = left_set.find(toRemove);
-            if (itL != left_set.end())
+            if (i < m)
             {
-                curr_sum -= itL->first;
-                left_set.erase(itL);
+                left_set.insert(window[i]);
+                curr_sum += window[i];
             }
             else
             {
-                auto itR = right_set.find(toRemove);
-                if (itR != right_set.end())
-                    right_set.erase(itR);
+                right_set.insert(window[i]);
             }
+        }
 
-            // 2. Aggiungiamo nums[i + dist]
-            if (i + dist < n)
-            {
-                right_set.insert({nums[i + dist], i + dist});
-            }
+        long long min_total_cost = (long long)cost_0 + curr_sum;
 
-            // 3. Bilanciamento chirurgico
-            // Se left_set è rimasto vuoto o ha bisogno di elementi
-            while (left_set.size() < m && !right_set.empty())
-            {
-                auto it = right_set.begin();
-                curr_sum += it->first;
-                left_set.insert(*it);
-                right_set.erase(it);
-            }
-
-            // 4. Swap veloce
-            if (m > 0 && !right_set.empty())
-            {
-                auto itL = prev(left_set.end());
-                auto itR = right_set.begin();
-                if (itL->first > itR->first)
-                {
-                    curr_sum = curr_sum - itL->first + itR->first;
-
-                    pair<int, int> valL = *itL;
-                    pair<int, int> valR = *itR;
-
-                    left_set.erase(itL);
-                    right_set.erase(itR);
-
-                    left_set.insert(valR);
-                    right_set.insert(valL);
-                }
-            }
-
-            min_total_cost = min(min_total_cost, cost_0 + nums[i] + curr_sum);
+        for (int i = 1, k = dist + 2; k < n; ++k, ++i)
+        {
+            remove(nums[i], left_set, right_set, curr_sum, m);
+            add(nums[k], left_set, right_set, curr_sum, m);
+            min_total_cost = min(min_total_cost, (long long)nums[0] + curr_sum);
         }
 
         return min_total_cost;
