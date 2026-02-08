32754dda181533a9d7faabd0c794f1ec06a03fb4
Paolo Inglese <p.inglese@outlook.com>
2026-02-08 11:00:37 +0100

    problem 110

---
+++ easy/110_balanced_binary_tree/main.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+#include <algorithm>
+
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    int checkSubTreeHeight(TreeNode *node)
+    {
+        if (node == nullptr)
+            return 0;
+
+        int leftHeight = checkSubTreeHeight(node->left);
+        if (leftHeight == -1)
+            return -1;
+        int rightHeight = checkSubTreeHeight(node->right);
+        if (rightHeight == -1)
+            return -1;
+
+        if (abs(leftHeight - rightHeight) > 1)
+        {
+            return -1;
+        }
+
+        return max(leftHeight, rightHeight) + 1;
+    }
+
+    bool isBalanced(TreeNode *root)
+    {
+        return checkSubTreeHeight(root) != -1;
+    }
+};
