7e19eac634aebcdb65694cce9346cbe88c54cf84
pinglese <p.inglese@outlook.com>
2026-03-25 16:19:01 +0100

    problem 8

--- medium/8_string_to_integer_atoi/main.py
+++ medium/8_string_to_integer_atoi/main.py
@@ -4,37 +4,44 @@ class Solution:
         seen_any = False
         seen_digit = False
         prev_sign = False
+        stop = False
         for c in s:
-            if c == " " and not seen_any:
-                continue
+            if c == " ":
+                if not seen_any:
+                    continue
+                else:
+                    break
             try:
                 val = int(c)
-                if val == 0 and not seen_digit:
-                    continue
                 out += c
                 seen_digit = True
+                seen_any = True
                 continue
             except:
                 if c in ["+", "-"]:
                     if not seen_digit and not prev_sign:
-                        out += c
+                        if c == "-":
+                            out += c
                         prev_sign = True
                     else:
-                        break
+                        stop = True
                 else:
-                    break
+                    stop = True
+
+            seen_any = True
+
+            if stop:
+                break
 
         if out == "":
             return 0
         else:
-            out = int(out)
+            try:
+                out = int(out)
+            except:
+                return 0
             if out < -(2**31):
                 return -(2**31)
             elif out > 2**31 - 1:
                 return 2**31 - 1
             return out
-
-
-if __name__ == "__main__":
-    sol = Solution()
-    print(sol.myAtoi("  -42"))
